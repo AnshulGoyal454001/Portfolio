fb55456140a20572a6f58e2c8d43fe8b9a0e4ec2
AnshulGoyal454001 <35340038+AnshulGoyal454001@users.noreply.github.com>
2020-07-01 00:28:47 +0530

    Add files via upload

---
+++ build/precache-manifest.a92798fb48f447ee658672f36ca74624.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa69e9e66b4ecaf9da2fb9241556482a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d3693355c131e69873",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "1305c3f0215cb2dec337",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56d3693355c131e69873",
+    "url": "/static/js/2.df6785bd.chunk.js"
+  },
+  {
+    "revision": "2d44b0d619a12c1417728a085056f2e7",
+    "url": "/static/js/2.df6785bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1305c3f0215cb2dec337",
+    "url": "/static/js/main.87c7e947.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "f484bf01b1aedfef295448eb6ca0cc0b",
+    "url": "/static/media/Chat.f484bf01.png"
+  },
+  {
+    "revision": "794a9a1b2112aef6d0eb9dfe5804da4e",
+    "url": "/static/media/algoViz.794a9a1b.png"
+  },
+  {
+    "revision": "781ee08c5665c825c1d9f3ee1ce62b22",
+    "url": "/static/media/anshul.781ee08c.jpg"
+  },
+  {
+    "revision": "83cef05c09218d86bdf31ba99bcfdd8a",
+    "url": "/static/media/coronaTracker.83cef05c.png"
+  },
+  {
+    "revision": "62ee85f975867b5288b6eaf94ac0a061",
+    "url": "/static/media/gameZone.62ee85f9.jpeg"
+  },
+  {
+    "revision": "76f98a6943a1639d23096cd32df32d8a",
+    "url": "/static/media/isoi.76f98a69.png"
+  },
+  {
+    "revision": "076d7c941c847d22f7fcd11dfc64fab0",
+    "url": "/static/media/oAuth.076d7c94.png"
+  },
+  {
+    "revision": "68729bfde6294f2165d3bbd280e4d467",
+    "url": "/static/media/restApi.68729bfd.png"
+  },
+  {
+    "revision": "c862d0b5ca3a1b646fc2259290729ae3",
+    "url": "/static/media/vac.c862d0b5.png"
+  },
+  {
+    "revision": "a0139d23892b7f0e31622d8c430a5d92",
+    "url": "/static/media/vitGrade.a0139d23.jpg"
+  },
+  {
+    "revision": "bae52dc1009df47c4fef35fd058f7ccc",
+    "url": "/static/media/watermark.bae52dc1.png"
+  },
+  {
+    "revision": "9efb6827f7620f5e8c9a10b3d7bb49bf",
+    "url": "/static/media/womenSafety.9efb6827.jpg"
+  }
+]);
